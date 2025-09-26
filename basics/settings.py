831baa7aa2addc8d0f6e8ec0b9b8f63ecc2a4973
Suraj Balkrishna Patil <surajapatil2002@gmail.com>
2025-09-26 13:10:27 +0530

    Update settings.py
    added debug to false

--- basics/settings.py
+++ basics/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-f2c+zh*4da_)^n%x+vzxuqzscv3b@^1^q*dl)11pk9-(y*8!)w"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['d-auth-delta.vercel.app']
 
 
 # Application definition
