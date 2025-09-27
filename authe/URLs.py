19e0e153a2c6558e5003a2a279f044c8b0f21270
Suraj Balkrishna Patil <surajapatil2002@gmail.com>
2025-09-27 17:25:25 +0530

    Update URLs.py

--- authe/URLs.py
+++ authe/URLs.py
@@ -18,7 +18,7 @@ urlpatterns = [
          auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"), 
          name="password_reset_done"),
 
-    path("reset/<uidb64>/set-password/", 
+    path("reset/<uidb64>/<token>/", 
          auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), 
          name="password_reset_confirm"),
 
