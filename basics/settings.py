2fb7c3d2161f18d44a8f0d0683631685cc088d2e
Suraj Balkrishna Patil <surajapatil2002@gmail.com>
2025-09-27 15:38:59 +0530

    Update settings.py

--- basics/settings.py
+++ basics/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "basics.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER":"postgres",
+        "PASSWORD":"VRPUpLCpRSCridyPDKnSsndqfbTTewML",
+        "HOST":"centerbeam.proxy.rlwy.net",
+        "PORT":"31757",
     }
 }
 
